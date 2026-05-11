c926572b5db63ce577d9ad9fc70529afbd17c5bb
CesarAle18 <cesaralebocal18@gmail.com>
2026-05-11 09:57:16 -0500

    fix

--- Project-IA-Intelligent-Task-Divider-Api/app/models/metrica.py
+++ Project-IA-Intelligent-Task-Divider-Api/app/models/metrica.py
@@ -1,4 +1,6 @@
+# pyrefly: ignore [missing-import]
 from sqlalchemy import Column, Integer, Float, String, DateTime
+# pyrefly: ignore [missing-import]
 from sqlalchemy.sql import func
 from app.database import Base
 
